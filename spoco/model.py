e378e7bee9eebb9a20756fd00986c23a812a30cb
Adrian Wolny <adrian.wolny@iwr.uni-heidelberg.de>
2022-01-27 21:18:03 +0100

    minor update

--- spoco/model.py
+++ spoco/model.py
@@ -116,13 +116,13 @@ class SpocoUNet(nn.Module):
     def __init__(self, net_f, net_g, m=0.999, init_equal=True):
         super(SpocoUNet, self).__init__()
 
-        self.f = net_f
-        self.g = net_g
+        self.net_f = net_f
+        self.net_g = net_g
         self.m = m
 
         if init_equal:
             # initialize g weights to be equal to f weights
-            for param_f, param_g in zip(self.f.parameters(), self.g.parameters()):
+            for param_f, param_g in zip(self.net_f.parameters(), self.net_g.parameters()):
                 param_g.data.copy_(param_f.data)  # initialize
                 param_g.requires_grad = False  # freeze g parameters
 
@@ -131,17 +131,17 @@ class SpocoUNet(nn.Module):
         """
         Momentum update of the g
         """
-        for param_f, param_g in zip(self.f.parameters(), self.g.parameters()):
+        for param_f, param_g in zip(self.net_f.parameters(), self.net_g.parameters()):
             param_g.data = param_g.data * self.m + param_f.data * (1. - self.m)
 
     def forward(self, im_f, im_g):
         # compute f-embeddings
-        emb_f = self.f(im_f)
+        emb_f = self.net_f(im_f)
 
         # compute g-embeddings
         with torch.no_grad():  # no gradient to g-embeddings
             self._momentum_update()  # momentum update of g
-            emb_g = self.g(im_g)
+            emb_g = self.net_g(im_g)
 
         return emb_f, emb_g
 
